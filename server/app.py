abcc65d9175d6342c3f51811e791f8753ab05022
Samson Njai <njaisamson@gmail.com>
2024-10-01 17:59:34 +0300

    creates the required views

--- server/app.py
+++ server/app.py
@@ -38,6 +38,32 @@ def find_earthquake(id):
             "message": f"Earthquake {id} not found."
         }
         return jsonify(error_message), 404
+    
+@app.route('/earthquakes/magnitude/<float:magnitude>')
+def find_minimum_magnitude(magnitude):
+    minimum_magnitude_earthquakes = Earthquake.query.filter(Earthquake.magnitude>=magnitude).all()
+    
+    if minimum_magnitude_earthquakes:
+        response_body = {
+            "count": len(minimum_magnitude_earthquakes),
+            "quakes": [
+                {
+                    "id": earthquake.id,
+                    "location": earthquake.location,
+                    "magnitude": earthquake.magnitude,
+                    "year": earthquake.year                    
+                } for earthquake in minimum_magnitude_earthquakes
+            ]
+            
+        } 
+        return jsonify(response_body), 200
+    else:
+        response_body = {
+            "count": 0,
+            "quakes": []
+        } 
+        return jsonify(response_body), 200      
+    
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
